88261166470386199977582f500984bc56aa5e37
filippo <roffo@MacBook-Pro-di-filippo.local>
2025-01-10 23:50:23 +0100

    Initial commit with project files

---
+++ src/helpers.ts
@@ -0,0 +1,10 @@
+import { html, type TemplateResult } from 'lit'
+
+export function safeRender<T> (renderFn: () => T): T | TemplateResult {
+  try {
+    return renderFn()
+  } catch (e) {
+    console.error('clock-weather-card - Error while rendering clock-weather-card component:', e)
+    return html``
+  }
+}
